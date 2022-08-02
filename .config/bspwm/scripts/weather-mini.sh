3a0bce23e52a3012c913a97a9a41801c281fa6b9
rxyvxr <109017615+rxyvxr@users.noreply.github.com>
2022-08-02 14:48:10 +0800

    Update weather-mini.sh

--- .config/bspwm/scripts/weather-mini.sh
+++ .config/bspwm/scripts/weather-mini.sh
@@ -3,4 +3,4 @@
 #curl -sS "wttr.in/38.906551,111.2222683?format="%c+%t+%h""
 #curl -sS "https://wttr.in/?format=3"
 
-echo "$(curl -sS "https://wttr.in/Mexico?format=3")"
+echo "$(curl -sS "https://wttr.in/Pili?format=3")"
