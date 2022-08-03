aed8cb4ed1abf32818a3e5033b881ac1528a3967
rxyvxr <109017615+rxyvxr@users.noreply.github.com>
2022-08-03 14:05:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,9 +12,6 @@ playerctl mpd ncmpcpp mpc picom-arian8j2-git xtitle termite betterlockscreen \
 nerd-fonts-jetbrains-mono ttf-jetbrains-mono papirus-icon-theme ttf-inconsolata feh
 
 #install dotfiles
-cd ~
-git clone https://github.com/gh0stzk/dotfiles.git
-cd dotfiles
 mkdir ~/.config
 mkdir ~/.local
 chown $(whoami): ~/.local
